14c12173956dd42e874076189b2171b403124df6
hiroyuki.akimoto <dokomademo1124@gmail.com>
2024-02-05 00:13:56 +0900

    update: githubログインをしてアバターURLを入れる

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
+  const user = session?.user;
+  console.log(user);
+
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
@@ -19,7 +24,7 @@ const Header = () => {
           </Link>
 
           <Link href={`/profile`}>
-            <Image width={50} height={50} alt="profile_icon" src={"/default_icon.png"} />
+            <Image width={50} height={50} alt="profile_icon" src={user?.image || "/default_icon.png"} />
           </Link>
         </div>
       </nav>
